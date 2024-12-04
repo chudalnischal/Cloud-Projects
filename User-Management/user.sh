af5b77e18356d057d1784f7c7cd657d1b3966b25
Nischal CHudal <chudalnischal7@gmail.com>
2024-12-03 21:16:29 -0500

    updating the code

--- User-Management/user.sh
+++ User-Management/user.sh
@@ -1,65 +1,72 @@
 #!/bin/bash
  
+echo "This is the shell script for user modification"
 
-echo " this is the shell script for user moification"
+echo "What do you want to do with the user?"
+echo "1. Create a new user"
+echo "2. Delete an existing user"
+echo "3. Modify an existing user"
+echo "4. Backup"
+echo "5. Exit"
 
-
-echo " what do you want to do with the user?"
-echo " 1. create a new user"
-echo " 2. delete an existing user"
-echo " 3. modify an existing user"
-echo " 4. Backup"
-echo " 5. Exit"
-
-read -p "Enter your choice:(1 to 5)" choice
-userChoice=$choice
+read -p "Enter your choice (1 to 5): " userChoice
 
 if [ $userChoice -eq 1 ]; then
-  echo "creating a new user"
-  #p flag is used to prompt the user to enter the username
-  read -p "enter the username:" username
-  echo "you entered $username" 
-  read -p " do you have any group name in mind? (y/n)" 
-  newChoice =$choice
-  if ["$choice" == "y"]; then
-    read -p "enter the group name:" groupname
-    usermod -a -G $groupname $username
+  echo "Creating a new user"
+  read -p "Enter the username: " username
+  echo "You entered $username" 
+  read -p "Do you have any group name in mind? (y/n): " newChoice
+  if [ "$newChoice" == "y" ]; then
+    read -p "Enter the group name: " groupname
+    usermod -a -G "$groupname" "$username"
   else
-    read -p "enter a new group name to create:" newgroupname
-    echo "creating a new group $newgroupname"
-    groupadd $newgroupname
-    usermod -a -G $newgroupname $username
+    read -p "Enter a new group name to create: " newgroupname
+    echo "Creating a new group $newgroupname"
+    sudo groupadd "$newgroupname"
+    sudo usermod -a -G "$newgroupname" "$username"
   fi
   
 elif [ $userChoice -eq 2 ]; then
-  echo "deleting an existing user"
-  read -p "enter the username to delete:" username
-  userdel -r $username
-  echo "user $username has been deleted"
+  echo "Deleting an existing user"
+  read -p "Enter the username to delete: " username
+  if id "$username" &>/dev/null; then
+    sudo userdel -r "$username"
+    echo "User $username has been deleted"
+  else
+    echo "User $username does not exist"
+    exit 1
+  fi
   
 elif [ $userChoice -eq 3 ]; then
-  echo "modifying an existing user"
-  read -p "enter the username to modify:" username
-  read -p "enter the new username:" newusername
-  usermod -l $newusername $username
-  echo "username has been changed from $username to $newusername"
+  echo "Modifying an existing user"
+  read -p "Enter the username to modify: " username
+  if id "$username" &>/dev/null; then
+    read -p "Enter the new username: " newusername
+    usermod -l "$newusername" "$username"
+    echo "Username has been changed from $username to $newusername"
+  else
+    echo "User $username does not exist"
+    exit 1
+  fi
   
 elif [ $userChoice -eq 4 ]; then
-  echo "backup"
-  read -p "enter the username to backup:" username
-  read -p "enter the backup location:" backuplocation
-  tar -cvf $backuplocation/$username.tar /home/$username
-  echo "backup has been created"
+  echo "Backup"
+  read -p "Enter the username to backup: " username
+  if id "$username" &>/dev/null; then
+    read -p "Enter the backup location: " backuplocation
+    mkdir -p "$backuplocation"
+    tar -cvf "$backuplocation/$username.tar" "/home/$username"
+    echo "Backup has been created"
+  else
+    echo "User $username does not exist"
+    exit 1
+  fi
   
 elif [ $userChoice -eq 5 ]; then
-  echo "exiting the script"
+  echo "Exiting the script"
   exit 0
+
 else
-  echo "invalid choice"
+  echo "Invalid choice"
   exit 1
-
-fi 
-
-
-
-
+fi
